c8c65610cca1c19a251b601d2929df448eed08d7
Danny Guo <dannyguo91@gmail.com>
2019-09-07 13:17:48 -0400

    Remove the email checker
    The API for pastes and breaches now requires authentication with an API
    key that costs $3.50 a month.
    
    https://www.troyhunt.com/authentication-and-the-have-i-been-pwned-api/
    
    The password API is unaffected.

--- src/pwned.js
+++ src/pwned.js
@@ -1,55 +1,5 @@
 import sha1 from 'sha1';
 
-const API = 'https://haveibeenpwned.com/api/v2';
-export const RATE_LIMIT_MS = 1510;
-
-export async function checkForBreaches(email) {
-    const response = await fetch(
-        `${API}/breachedaccount/${encodeURIComponent(email)}`
-    );
-    if (response.status === 200) {
-        const json = await response.json();
-        return json;
-    } else if (response.status === 404) {
-        return [];
-    } else {
-        return Promise.reject({responseCode: response.status});
-    }
-}
-
-export async function checkForPastes(email) {
-    const response = await fetch(
-        `${API}/pasteaccount/${encodeURIComponent(email)}`
-    );
-    if (response.status === 200) {
-        const json = await response.json();
-        return json;
-    } else if (response.status === 404) {
-        return [];
-    } else {
-        return Promise.reject({responseCode: response.status});
-    }
-}
-
-export function getPasteUrl(source, id) {
-    switch (source) {
-        case 'Pastebin':
-            return `https://pastebin.com/${id}`;
-        case 'Slexy':
-            return `https://slexy.org/view/${id}`;
-        case 'Ghostbin':
-            return `https://ghostbin.com/paste/${id}`;
-        case 'JustPaste':
-            return `https://justpaste.it/${id}`;
-        case 'AdHocUrl':
-        case 'OptOut':
-        case 'Pastie':
-        case 'QuickLeak':
-        default:
-            return '';
-    }
-}
-
 export async function pwnedPassword(password) {
     const targetHash = sha1(password).toUpperCase();
 
